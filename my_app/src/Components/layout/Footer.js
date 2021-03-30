f53cc5c6f8ddd807ab004b191b95fcf8589826ea
HaithamOumerzoug <haithamoumerzoug31@gmail.com>
2021-03-30 17:53:04 +0100

    class => className (JSX)

--- my_app/src/Components/layout/Footer.js
+++ my_app/src/Components/layout/Footer.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Footer =(props)=>{
     const branding = props.branding;
     return (
-        <div class="card text-center text-white bg-dark mt-5">
-            <div class="card-header">
+        <div className="card text-center text-white bg-dark mt-5">
+            <div className="card-header">
                 <a href="/" className="text-decoration-none display-5 font-weight-bold" style={{color:'white'}}>{branding}</a>
             </div>
             <div class="card-body">
-                <p class="card-text">Created by <a className="text-decoration-none" href="https://haitham-oumerzoug.com/">Haitham OUMERZOUG</a></p>
+                <p className="card-text">Created by <a className="text-decoration-none" href="https://haitham-oumerzoug.com/">Haitham OUMERZOUG</a></p>
             </div>
-            <div class="card-footer text-muted">
-            <a href="https://github.com/HaithamOumerzoug" class="text-decoration-none text-muted font-weight-bold">GitHub</a>
+            <div className="card-footer text-muted">
+            <a href="https://github.com/HaithamOumerzoug" className="text-decoration-none text-muted font-weight-bold">GitHub</a>
             </div>
         </div>
     )
