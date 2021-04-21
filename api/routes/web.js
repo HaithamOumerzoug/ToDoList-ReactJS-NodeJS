6532c978a423e2e16696958f59fb51cbb87d6da5
HaithamOumerzoug <haithamoumerzoug31@gmail.com>
2021-04-21 22:59:16 +0000

    change api router

--- api/routes/web.js
+++ api/routes/web.js
@@ -4,13 +4,13 @@ const { body } = require('express-validator');
 const todolistController=require('../Controllers/ToDoListController');
 const listValidator = require('../middleware/listValidator');
 
-router.get('/',todolistController.home);
-router.get('/alllists',todolistController.getAll)
-router.get('/:id',todolistController.getList)
-//router.get('/addlist')
-router.post('/addlist',listValidator.ToDolistValidator,todolistController.addList)
-router.delete('/:id',todolistController.deleteList)
-router.put('/changecheck/:id',listValidator.ToDolistValidator,todolistController.changecheck)
-router.put('/:id',todolistController.updateList)
+router.get('/api',todolistController.home);
+router.get('/api/alllists',todolistController.getAll)
+router.get('/api/:id',todolistController.getList)
+//router.get('/api/addlist')
+router.post('/api/addlist',listValidator.ToDolistValidator,todolistController.addList)
+router.delete('/api/:id',todolistController.deleteList)
+router.put('/api/changecheck/:id',listValidator.ToDolistValidator,todolistController.changecheck)
+router.put('/api/:id',todolistController.updateList)
 
 module.exports=router;
